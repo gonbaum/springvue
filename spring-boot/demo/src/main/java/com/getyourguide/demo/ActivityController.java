1913bd49615e256f6b16649a1e0a1d0cd033e602
Gonzalo Andres Garcia <nouvellegon@gmail.com>
2023-08-07 12:02:08 +0200

    feat: get by id

--- spring-boot/demo/src/main/java/com/getyourguide/demo/ActivityController.java
+++ spring-boot/demo/src/main/java/com/getyourguide/demo/ActivityController.java
@@ -3,6 +3,7 @@ package com.getyourguide.demo;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,9 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -24,31 +28,55 @@ public class ActivityController {
     public void debug(@RequestParam(name = "title", required = false, defaultValue = "NONE") String title, Model model) {
         try {
             model.addAttribute("title", title);
-            //create ObjectMapper instance
+            // create ObjectMapper instance
             ObjectMapper objectMapper = new ObjectMapper();
-            //read JSON file and convert to a list of activities
-            var file = resourceLoader.getResource("classpath:static/activities.json").getFile();
-            var activities = objectMapper.readValue(file, new TypeReference<List<Activity>>() {
-            });
+            // read JSON file and convert to a list of activities
+            List<Activity> activities = readActivitiesFromJsonFile();
             model.addAttribute("activities", activities);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     @GetMapping("/activities")
     public ResponseEntity<List<Activity>> activities() {
-        List<Activity> activities = null;
         try {
-            //create ObjectMapper instance
-            ObjectMapper objectMapper = new ObjectMapper();
-            //read JSON file and convert to a list of activities
-            var file = resourceLoader.getResource("classpath:static/activities.json").getFile();
-            activities = objectMapper.readValue(file, new TypeReference<List<Activity>>() {
-            });
-
+            // read JSON file and convert to a list of activities
+            List<Activity> activities = readActivitiesFromJsonFile();
             return ResponseEntity.ok(activities);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+     @GetMapping("/activities/{id}")
+    public ResponseEntity<Activity> getActivityById(@PathVariable long id) {
+        try {
+            List<Activity> activities = readActivitiesFromJsonFile();
+
+            // Find the activity with the given ID
+            Optional<Activity> matchedActivity = activities.stream()
+                    .filter(activity -> activity.getId() == id)
+                    .findFirst();
+
+            // Return the activity if found, or a 404 response if not found
+            if (matchedActivity.isPresent()) {
+                return ResponseEntity.ok(matchedActivity.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Activity> readActivitiesFromJsonFile() throws IOException {
+        // create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+        // read JSON file and convert to a list of activities
+        Resource resource = resourceLoader.getResource("classpath:static/activities.json");
+        InputStream inputStream = resource.getInputStream();
+        return objectMapper.readValue(inputStream, new TypeReference<List<Activity>>() {
+        });
     }
 }
