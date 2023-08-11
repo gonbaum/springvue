973ea6f8ffa942035c578d9f98aa3cf75440f2b0
Gonzalo Andres Garcia <nouvellegon@gmail.com>
2023-08-11 09:38:59 +0200

    feat(backend): basic error handling

--- server/src/main/java/com/getyourguide/activity/controller/ActivityController.java
+++ server/src/main/java/com/getyourguide/activity/controller/ActivityController.java
@@ -2,9 +2,11 @@ package com.getyourguide.activity.controller;
 
 import com.getyourguide.activity.model.Activity;
 import com.getyourguide.activity.service.ActivityService;
+import com.getyourguide.util.error.ErrorResponse;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,26 +20,44 @@ public class ActivityController {
 
   @GetMapping("/activities")
   public ResponseEntity<List<Activity>> activities() {
-    List<Activity> activities = activityService.getAllActivities();
-    return ResponseEntity.ok(activities);
+    try {
+      List<Activity> activities = activityService.getAllActivities();
+      return ResponseEntity.ok(activities);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 
   @GetMapping("/activities/{id}")
-  public ResponseEntity<Activity> getActivityById(@PathVariable long id) {
-    Optional<Activity> matchedActivity = activityService.getActivityById(id);
-    // Return the activity if found, or a 404 response if not found
-    return matchedActivity
-      .map(ResponseEntity::ok)
-      .orElse(ResponseEntity.notFound().build());
+  public ResponseEntity<?> getActivityById(@PathVariable long id) {
+    try {
+      Optional<Activity> matchedActivity = activityService.getActivityById(id);
+
+      if (matchedActivity.isPresent()) {
+        return ResponseEntity.ok(matchedActivity.get());
+      } else {
+        ErrorResponse errorResponse = new ErrorResponse(
+          "Activity not found",
+          HttpStatus.NOT_FOUND
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+      }
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 
   @GetMapping("/activities/search")
   public ResponseEntity<List<Activity>> searchActivitiesByTitle(
     @RequestParam(name = "title") String title
   ) {
-    List<Activity> matchedActivities = activityService.searchActivitiesByTitle(
-      title
-    );
-    return ResponseEntity.ok(matchedActivities);
+    try {
+      List<Activity> matchedActivities = activityService.searchActivitiesByTitle(
+        title
+      );
+      return ResponseEntity.ok(matchedActivities);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
   }
 }
---
+++ server/src/main/java/com/getyourguide/util/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.getyourguide.util.error;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+
+  private String message;
+  private HttpStatus status;
+
+  public ErrorResponse(String message, HttpStatus status) {
+    this.message = message;
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+}
